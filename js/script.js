23c87dffee9c406913e6efa25d928f89d2d715a9
Alex_W <alexcwinter7@gmail.com>
2023-08-30 17:06:25 -0500

    js fixed

--- js/script.js
+++ js/script.js
@@ -7,16 +7,14 @@ var tripInfo = function() {
     var totalBudget = Number(prompt("What is your total budget?"));
     var accommodation = Number(prompt("What are you accommodation costs?"));
     var numDays = Number(Prompt("How many days does your trip last?"));
-    calculateDailyBudget(totalBudget)
-    calculateDailyBudget(accommodation)
-    calculateDailyBudget(numDays)
-}
 
-var calculateDailyBudget = function() {
-    var daily = ((totalBudget - accommadation)/numDays).toFixed(2);
+    calculateDailyBudget(totalBudget, accommodation, numDays);
+};
+
+var calculateDailyBudget = function(totalBudget, accommodation, numDays) {
+    var daily = ((totalBudget - accommodation) / numDays).toFixed(2);
+
     dailyBudget.innertext(`you can spend $${daily} a day on food and fun!`)
-}
+};
 
-tripInfoButton.addEventListener("click", function() {
-    calculateDailyBudget()
-})
+tripInfoButton.addEventListener("click", tripInfo);
